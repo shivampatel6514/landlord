f5da506bb21035abeb0f6245c605269fb8068101
vermashankar <u.s.verma12345@gmail.com>
2023-09-13 00:22:46 +0530

    add json render

--- landlord/settings.py
+++ landlord/settings.py
@@ -88,9 +88,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ]
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        # Add other renderers if needed
+    ],
 }
 
 MEDIA_URL = '/media/'
